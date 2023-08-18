426365b899655f527bebcc05d0da2eb69255b36c
ChoiWheatley <chltmdgus604@naver.com>
2023-08-18 16:07:54 +0900

    ✅2812: 통과는 했어도 1832ms이다.

--- ChoiWheatley/p2812.py
+++ ChoiWheatley/p2812.py
@@ -1,7 +1,7 @@
-def sol(n: int, k: int) -> int:
+def sol(n: str, k: int) -> int:
     """n에서 k개의 숫자를 지웠을 때 얻을 수 있는 가장 큰 수를 출력한다"""
     stack = []
-    for digit in [int(c) for c in str(n)]:
+    for digit in (int(x) for x in n):
         while k > 0 and len(stack) > 0 and digit > stack[-1]:
             k -= 1
             stack.pop()
@@ -12,7 +12,10 @@ def sol(n: int, k: int) -> int:
     return int("".join([str(x) for x in stack]))
 
 
-_, k = [int(x) for x in input().split()]
-n = int(input())
+if __name__ == "__main__":
+    _, k = input().split()
+    n = input()
 
-print(sol(n, k))
+    k = int(k)
+
+    print(sol(n, k))
