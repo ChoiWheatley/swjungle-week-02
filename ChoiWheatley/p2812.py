d560e2fd31ba3fea04e23b4f67d5e53db48a259d
ChoiWheatley <chltmdgus604@naver.com>
2023-08-18 13:29:48 +0900

    ⌛️2812: 이게왜 시간초과?

---
+++ ChoiWheatley/p2812.py
@@ -0,0 +1,15 @@
+def sol(n: int, k: int) -> int:
+    """n에서 k개의 숫자를 지웠을 때 얻을 수 있는 가장 큰 수를 출력한다"""
+    stack = []
+    for digit in [int(c) for c in str(n)]:
+        while k > 0 and len(stack) > 0 and digit > stack[-1]:
+            k -= 1
+            stack.pop()
+        stack.append(digit)
+    return int("".join([str(x) for x in stack]))
+
+
+_, k = [int(x) for x in input().split()]
+n = int(input())
+
+print(sol(n, k))
