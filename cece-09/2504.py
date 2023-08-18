8273d2f91d35f5db6c62f6ed2df269da0c02d31a
cece <sojeong.lee017@gmail.com>
2023-08-18 11:02:07 +0900

    Accepted(2504)

--- cece-09/2504.py
+++ cece-09/2504.py
@@ -1,48 +1,44 @@
 S = input()
-STX = []
-# STY = []
-
-
-def empty():
-    return len(STX) == 0
-
+ST = []
 
 answer = 0
-last = -1
+tmp = 1
+prev = ""  # 직전 괄호를 검사
+is_VPS = True
 for i in range(len(S)):
     if S[i] == "(":
-        STX.append(S[i])
+        tmp *= 2
+        ST.append(S[i])
     if S[i] == "[":
-        STX.append(S[i])
+        tmp *= 3
+        ST.append(S[i])
     if S[i] == ")":
-        if empty():
-            print(0)
+        if len(ST) == 0:
+            is_VPS = False
             break
-        top = STX.pop()
-        if top == "(":
-            if last == 0:
-                answer = answer * 2
-            if last == 1:
-                answer = answer + 2
-            if last == -1:
-                answer = 2
-            last = 0
-        else:
-            print(0)
-            break
+        top = ST[-1]
+        if top == "(":  # 괄호 매칭되면 pop
+            ST.pop()
+            if prev == "(":  # () 매칭이면 answer로 더하기
+                answer += tmp
+        tmp //= 2  # 연산 완료되었으므로 // 2
     if S[i] == "]":
-        if empty():
-            print(0)
+        if len(ST) == 0:
+            is_VPS = False
             break
-        top = STX.pop()
+        top = ST[-1]
         if top == "[":
-            if last == 0:
-                answer = answer * 3
-            if last == 1:
-                answer = answer + 3
-            if last == -1:
-                answer = 3
-            last = 0
-        else:
-            print(0)
-            break
+            ST.pop()
+            if prev == "[":
+                answer += tmp
+        tmp //= 3
+    prev = S[i]
+
+
+if len(ST) > 0:  # 스택에 괄호가 남아있으면 not VPS
+    is_VPS = False
+
+if is_VPS:
+    print(answer)
+else:
+    print(0)
