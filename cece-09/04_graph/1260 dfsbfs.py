5ba4ba07b74e73562ae3d15cfd9570e1b42107a2
cece <sojeong.lee017@gmail.com>
2023-08-19 22:22:51 +0900

    Accept(1260)

--- cece-09/04_graph/1260 dfsbfs.py
+++ cece-09/04_graph/1260 dfsbfs.py
@@ -1,5 +1,6 @@
 import sys
 from collections import deque
+from heapq import heappush, heappop
 
 
 class Node:
@@ -16,26 +17,71 @@ class Edge:
         self.rp = None  # vr 가 같은 연결된 에지
 
 
-def dfs(v):
+def get_adjacents(v: int):
     global vtx, edg
-    visit[v] = 1  # 시작노드 방문
-    # cnt = 0
+    adj = []
+    # front
+    if vtx[v].fh != None:
+        e = vtx[v].fh  # front edge index
+        while e != None:
+            vf, vr = edg[e].vf, edg[e].vr  # v1 - v2
+            if visit[vr] == 0:
+                heappush(adj, vr)
+            e = edg[e].fp  # v와 연결된 다른 에지 검색
 
-    # front side
-    node = vtx[v]
-    edge = edg[node.fh]
-    while edge != None:
-        vf = edge.vf
-        if vf != None and visit[vf] == 0:
-            visit[vf] = 1
-            print(vf)
-            edge = vtx[vf].fh
+    # rear
+    if vtx[v].rh != None:
+        e = vtx[v].rh  # rear edge index
+        while e != None:
+            vf, vr = edg[e].vf, edg[e].vr   # v1 - v2
+            if visit[vf] == 0:
+                heappush(adj, vf)
+            e = edg[e].rp  # v와 연결된 다른 에지 검색
+
+    rtn = []  # 더 작은 정점
+    for _ in range(len(adj)):
+        v = heappop(adj)
+        rtn.append(v)
+    return rtn
 
 
+def bfs(s):
+    queue = deque([])
+
+    queue.append(s)  # 시작노드 방문
+    while queue:
+        v = queue.popleft()
+
+        if visit[v] == 0:
+            visit[v] = 1
+            print(v+1, end=' ')
+
+        adj = get_adjacents(v)
+
+        for a in adj:  # 인접 정점을 queue
+            queue.append(a)
+
+
+def dfs(s, cnt):
+    global visit
+
+    visit[s] = 1
+    print(s+1, end=' ')
+
+    if cnt == N:
+        return
+
+    adj = get_adjacents(s)
+    for a in adj:
+        if visit[a] == 0:
+            dfs(a, cnt+1)
+
+
+# main
 N, M, V = map(int, input().split())
 
-vtx: list[Node] = []
-edg: list[Edge] = []
+vtx = []
+edg = []
 visit = [0 for _ in range(N)]
 
 # data structure
@@ -57,5 +103,13 @@ for i in range(M):
     vtx[vr-1].rh = i
     edg.append(edge)
 
+# search
+dfs(V-1, 0)
+print()
 
-dfs(V-1)
+# visit 배열 초기화
+for i in range(N):
+    visit[i] = 0
+
+bfs(V-1)
+print()
