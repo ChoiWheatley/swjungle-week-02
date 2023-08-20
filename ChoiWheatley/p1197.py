9639d60a469768086c786d4d345bfd5d0b22e927
ChoiWheatley <chltmdgus604@naver.com>
2023-08-20 16:17:04 +0000

    ✅(1197): 최소 신장 트리 -- 서로소집합 개념을 활용하여 사이클 검사

--- ChoiWheatley/p1197.py
+++ ChoiWheatley/p1197.py
@@ -1,40 +1,64 @@
 """최소신장트리"""
 from typing import List, Tuple
-from sys import stdin, maxsize
+from sys import stdin
 
 
 MAXE = 100_000
 MAXV = 10_000
 
 
+class NotADisJointSet(Exception):
+    ...
+
+
+def get_parent(disjoint_set: List[int], idx: int) -> int:
+    if disjoint_set[idx] == idx:
+        # 자기자신이 루트(서로소집합 그 자체)인 경우
+        return idx
+    return get_parent(disjoint_set, disjoint_set[idx])
+
+
+def union_parent(disjoint_set: List[int], a, b):
+    """두 서로소 집합을 합친다. 만약 서로소 집합이 아닌 경우 에러를 발생시킨다."""
+    set_a = get_parent(disjoint_set, a)
+    set_b = get_parent(disjoint_set, b)
+    if set_a == set_b:
+        raise NotADisJointSet()
+    if set_a > set_b:
+        # 작은 인덱스가 부모가 되도록
+        set_a, set_b = set_b, set_a
+    disjoint_set[set_b] = set_a
+
+
 def sol(G: List[Tuple[int, int, int]]) -> int:
-    """순회를 돌면서 매번 최소 가중치 간선을 찾아 추가하는 알고리즘
+    """
+    순회를 돌면서 매번 최소 가중치 간선을 찾아 추가하는 알고리즘
     가중치값만 구하면 되므로 최소신장그래프는 작성하지 않는다.
+
     근데, 순환탐지를 어떻게 하지? => 간선을 추가할 때 중복된 정점이 있다면 순환임.
-    - G: 연결리스트 형식의 그래프, 각 원소는 (시작 정점번호, 인접 정점번호, 가중치)로 이루어져 있다. 1부터 센다.
+
+    근데, 중복된 정점을 어떻게 찾지? => Union Find
+
+    - G: 연결리스트 형식의 그래프, 각 원소는 (시작 정점번호, 인접 정점번호, 가중치)로 이루어져 있다.
 
     """
-    parent = [i for i in range(len(G))]  # 순환탐지용
-    G[1:].sort(key=lambda e: e[1])
-    parent[G[1][1]] = G[1][0]  # 루트노드를 G[1][0]으로 설정한다.
-    ret = G[1][2]  # 가중치합
-    for u, v, weight in G[2:]:
-        if parent[u] == G[1][0] and parent[v] == G[1][0]:
-            # cycle!
+    disjoint_set = [i for i in range(MAXV + 1)]  # 순환탐지용
+    ret = 0
+    G.sort(key=lambda e: e[2])
+
+    for u, v, w in G:
+        try:
+            union_parent(disjoint_set, u, v)
+            ret += w
+        except NotADisJointSet:
             continue
-
-        ret += weight
-
-        # 루트노드를 parent로 지정하는 것으로 간편하게 순환검사를 할 수 있다.
-        parent[u] = G[1][0]
-        parent[v] = G[1][0]
 
     return ret
 
 
 if __name__ == "__main__":
     V, E = (int(x) for x in stdin.readline().split())
-    G: List[Tuple[int, int, int]] = [(maxsize, maxsize, maxsize)]  # 0인덱스는 사용하지 않는다
+    G: List[Tuple[int, int, int]] = []
 
     for _ in range(E):
         a, b, c = (int(x) for x in stdin.readline().split())
