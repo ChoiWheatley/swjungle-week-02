14e892b062b16678ea8d5af02ac9d9196b4900df
cece <sojeong.lee017@gmail.com>
2023-08-21 23:57:32 +0900

    Accept(7569)

--- cece-09/06_bfs/2665 미로.py
+++ cece-09/06_bfs/2665 미로.py
@@ -1,42 +1,55 @@
 import sys
 from collections import deque
+from heapq import heappop, heappush
 
 inf = float('inf')
+
 N = int(input())
 maze = [list(map(int, sys.stdin.readline().strip())) for _ in range(N)]
 dist = [[inf for _ in range(N)] for _ in range(N)]
-dist2 = [[inf for _ in range(N)] for _ in range(N)]
+order = [[0 for _ in range(N)] for _ in range(N)]
 direct = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 
-def find_white(i, j):
-    # (i, j)에서 가장 가까이에 있는 흰 방을 찾음
-    black_cnt = 0
-    queue = deque()
-    queue.append((i, j))
-    while queue:
-        fi, fj = queue.popleft()
+# def find_white(i, j):
+#     # (i, j)에서 가장 가까이에 있는 흰 방을 찾음
+#     black_cnt = 0
+#     queue = deque()
+#     queue.append((i, j))
+#     dist2[i][j] = 0
+#     while queue:
+#         fi, fj = queue.popleft()
 
-        if (fi != i or fj != j) and maze[fi][fj] == 1:
-            break
+#         if (fi != i or fj != j) and maze[fi][fj] == 1:
+#             print(fi, fj, ":", dist2[fi][fj])
+#             break
 
-        for dir in direct:
-            di, dj = fi+dir[0], fj+dir[1]
-            if 0 <= di < N and 0 <= dj < N:
-                if dist2[di][dj] == inf:
-                    queue.append((di, dj))
-                dist2[di][dj] = min(dist2[di][dj], dist2[fi][fj]+1)
+#         for dir in direct:
+#             di, dj = fi+dir[0], fj+dir[1]
+#             if 0 <= di < N and 0 <= dj < N:
+#                 if dist2[di][dj] == inf:
+#                     queue.append((di, dj))
+#                 dist2[di][dj] = min(dist2[di][dj], dist2[fi][fj]+1)
+#                 # if (di != i or dj != j) and maze[fi][fj] == 1:
+#                 #     print(di, dj)
+#                 #     return
 
 
 def bfs(i, j):
-    queue = deque()
-    queue.append((i, j))
+    pq = []
+    heappush(pq, (-1, i, j))
     dist[i][j] = 0
     # 검색하면서 최대로 갈 수 있는 좌표를 저장
     max_dist_coord = (0, 0)
     max_dist = 0
-    while queue:
-        fi, fj = queue.popleft()
+
+    # 방문 순서
+    cnt = 0
+    while pq:
+        col, fi, fj = heappop(pq)
+        order[fi][fj] = cnt
+        cnt += 1
+        print(f"({fi}, {fj}) : {'white' if col < 0 else 'black'}")
 
         if dist[fi][fj] > max_dist:
             max_dist_coord = (fi, fj)
@@ -44,9 +57,10 @@ def bfs(i, j):
 
         for dir in direct:
             di, dj = fi+dir[0], fj+dir[1]
-            if 0 <= di < N and 0 <= dj < N and maze[di][dj] == 1:
+            if 0 <= di < N and 0 <= dj < N:
+                color = maze[di][dj]
                 if dist[di][dj] == inf:
-                    queue.append((di, dj))
+                    heappush(pq, (-color, di, dj))
                 dist[di][dj] = min(dist[di][dj], dist[fi][fj]+1)
 
     print(max_dist_coord, ":", max_dist)
@@ -62,4 +76,5 @@ def print_mtx(mtx):
 
 # print_mtx(maze)
 bfs(0, 0)
-print_mtx(dist)
+# find_white(4, 1)
+print_mtx(order)
