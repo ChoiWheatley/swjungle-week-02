b23c92aab51b2d22c6c96d2eb758bee8081a8e5a
cece <sojeong.lee017@gmail.com>
2023-08-22 15:00:29 +0900

    Accept(2665)

--- cece-09/06_bfs/2665 미로.py
+++ cece-09/06_bfs/2665 미로.py
@@ -2,49 +2,50 @@ import sys
 from collections import deque
 from heapq import heappop, heappush
 
-# ! 아직 못 푼 문제
-
-inf = float('inf')
-
-N = int(input())
-maze = [list(map(int, sys.stdin.readline().strip())) for _ in range(N)]
-dist = [[inf for _ in range(N)] for _ in range(N)]
-order = [[0 for _ in range(N)] for _ in range(N)]
-direct = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+'''
+주요 반례
+벽을 뚫을 필요가 없는데, 벽을 뚫는 것이 최단거리인 경우
+이를 위해 weight이 작은 쪽을 먼저 탐색하는 pq가 필요
+5
+10111
+10101
+10101
+10101
+11101
+wrong: 1
+right: 0
+'''
 
 
 def bfs(i, j):
     pq = []
-    heappush(pq, (-1, i, j))
+    heappush(pq, (0, i, j))  # 필수
     dist[i][j] = 0
 
-    # 방문 순서
-    cnt = 0
     while pq:
-        col, fi, fj = heappop(pq)
-        order[fi][fj] = cnt
-        cnt += 1
-        print(f"({fi}, {fj}) : {'white' if col < 0 else 'black'}")
-
-        if fi == N-1 and fj == N-1:
-            break
+        _, fi, fj = heappop(pq)
 
         for dir in direct:
             di, dj = fi+dir[0], fj+dir[1]
             if 0 <= di < N and 0 <= dj < N:
-                color = maze[di][dj]
                 if dist[di][dj] == inf:
-                    heappush(pq, (-color, di, dj))
-                    maze[di][dj] = -1
-                dist[di][dj] = min(dist[di][dj], dist[fi][fj]+1)
+                    heappush(pq, (dist[fi][fj] + cost[di][dj], di, dj))
+                dist[di][dj] = min(dist[di][dj], dist[fi][fj] + cost[di][dj])
 
 
-def print_mtx(mtx):
-    for i in range(N):
-        for j in range(N):
-            print(f"{mtx[i][j]:3}", end=' ')
-        print()
+inf = float('inf')
 
+N = int(input())
+cost = [list(map(int, sys.stdin.readline().strip())) for _ in range(N)]
+dist = [[inf for _ in range(N)] for _ in range(N)]
+direct = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+for i in range(N):
+    for j in range(N):
+        if cost[i][j] == 1:
+            cost[i][j] = 0
+        else:
+            cost[i][j] = 1  # weight 반전
 
 bfs(0, 0)
-print_mtx(order)
+print(dist[N-1][N-1])
